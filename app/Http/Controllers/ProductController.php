b79568a27cf4e71d51055972ece52de557c54f05
Fabian Suarez <fsuarez120@unab.edu.co>
2024-03-05 20:25:41 -0500

    trabajo en clase 05-03-2024

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    
+    function index(){
+        echo "estmos en el indice de proudctos";
+    }
+
+}
